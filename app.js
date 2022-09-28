dbf4ee34e72b7a2dd2cd4459e04f4d37152432b5
Oumaima <oumamem@gmail.com>
2022-09-28 17:25:13 +0200

    nodemon install

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hello');
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => res.send('Hello, Express'))
+
+app.listen(port, () => console.log('Notre application Nodejs démarrre ! '))
